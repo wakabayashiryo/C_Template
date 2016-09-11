73339caf20ef0a94adab192b05f703cdc95cfa63
EVAOTA <nervevawaka@ezweb.ne.jp>
2016-09-11 19:24:13 +0900

    READMEの記述

--- Debug/main.d
+++
@@ -1,5 +0,0 @@
-Debug/main.o: Src/main.c Inc/main.h lib/sample/Inc/sample.h
-
-Inc/main.h:
-
-lib/sample/Inc/sample.h:
--- Debug/sample.d
+++
@@ -1,3 +0,0 @@
-Debug/sample.o: lib/sample/Src/sample.c lib/sample/Inc/sample.h
-
-lib/sample/Inc/sample.h:
